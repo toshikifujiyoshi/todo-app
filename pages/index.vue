4fa64e5a31920e38a581e9684d79bcd23ec96575
toshikifujiyoshi <toshikifujiyoshi1023@gmail.com>
2024-01-21 21:29:17 +0900

    ✨ todoの削除機能実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { initializeApp } from 'firebase/app'
-import { getFirestore, getDocs, collection } from 'firebase/firestore'
+import { getFirestore, getDocs, collection, doc, deleteDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDghhPGaW0nCEm6Hjq2UrX5fBRK6Ikf9_U',
@@ -11,31 +11,45 @@ const firebaseConfig = {
   appId: '1:503092853490:web:9293af402fc98b0d2bee6b',
   measurementId: 'G-QW7NGBKF7K'
 }
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
 definePageMeta({
-  // レイアウト選択(ケバブケース)
   layout: 'is-sidebar'
 })
 
 const isOpen = ref(false)
 
-const todos = ref([]) // todoListをrefで宣言
+const todos = ref<DocumentData[]>([])
 
+// データの取得
 const querySnapshot = await getDocs(collection(db, 'todos'))
 querySnapshot.forEach(doc => todos.value.push({ field: doc.data(), id: doc.id }))
+
+// todo を削除する関数
+const deleteTodo = async (todoId: string) => {
+  try {
+    // todoId に対応するドキュメントを削除
+    await deleteDoc(doc(db, 'todos', todoId))
+
+    // 削除後、todos 配列からも削除
+    todos.value = todos.value.filter(todo => todo.id !== todoId)
+  } catch (error) {
+    console.error('Error deleting todo:', error)
+  }
+}
 </script>
+
 <template>
   <div class="p-20 h-[100%] w-[100%] bg-slate-400">
     <div class="bg-white rounded-md p-4 text-lg">
       <h2 class="font-bold mb-2">
         todo一覧
       </h2>
-      <div v-for="todo in todos" :key="todo" class="flex justify-between p-2 border-t">
+      <div v-for="todo in todos" :key="todo.id" class="flex justify-between p-2 border-t">
         <p>{{ todo.field.todoTitle }}</p>
-        <UButton label="削除する" color="rose" variant="solid" @click="delateTodo" />
+        <UButton label="削除する" color="rose" variant="solid" @click="() => deleteTodo(todo.id)" />
       </div>
     </div>
   </div>
